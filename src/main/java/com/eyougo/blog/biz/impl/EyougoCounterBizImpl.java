b20fc2c2262f579c5cdb15a4d56c6f479c3d685f
eyougo <eyougo@gmail.com>
2013-04-19 00:08:02 +0800

    加计数开关控制显示

--- src/main/java/com/eyougo/blog/biz/impl/EyougoCounterBizImpl.java
+++ src/main/java/com/eyougo/blog/biz/impl/EyougoCounterBizImpl.java
@@ -1,5 +1,7 @@
 package com.eyougo.blog.biz.impl;
 
+import org.apache.commons.lang3.BooleanUtils;
+
 import com.eyougo.blog.biz.CounterBiz;
 import com.eyougo.blog.biz.EyougoCounterBiz;
 
@@ -7,6 +9,7 @@ public class EyougoCounterBizImpl implements EyougoCounterBiz {
 	private CounterBiz counterBiz;
 	private String username;
 	private String password;
+	private String counterSwitch;
 	
 	public void setCounterBiz(CounterBiz counterBiz) {
 		this.counterBiz = counterBiz;
@@ -20,6 +23,10 @@ public class EyougoCounterBizImpl implements EyougoCounterBiz {
 		this.password = password;
 	}
 
+	public void setCounterSwitch(String counterSwitch) {
+		this.counterSwitch = counterSwitch;
+	}
+
 	@Override
 	public boolean counter() {
 		return counterBiz.counter(username, password);
@@ -27,7 +34,9 @@ public class EyougoCounterBizImpl implements EyougoCounterBiz {
 
 	@Override
 	public String getCounterInfo() {
-
+		if (!BooleanUtils.toBoolean(counterSwitch)){
+			return "0:0:0:0:";
+		}
 		return counterBiz.getCounterInfo(username, password);
 	}
 
