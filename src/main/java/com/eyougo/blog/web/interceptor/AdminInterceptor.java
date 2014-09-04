127a04f4897baed6fb9848448010f9657e34eede
eyougo <meihongyuan@163.com>
2014-09-04 23:11:31 +0800

    后台

--- src/main/java/com/eyougo/blog/web/interceptor/AdminInterceptor.java
+++ src/main/java/com/eyougo/blog/web/interceptor/AdminInterceptor.java
@@ -12,16 +12,16 @@ public class AdminInterceptor extends HandlerInterceptorAdapter{
 	@Override
 	public boolean preHandle(HttpServletRequest request,
 			HttpServletResponse response, Object handler) throws Exception {
-//		String uri = request.getRequestURI();
-//		String redirectLocation = request.getContextPath()+"/admin/login";
-//        if (StringUtils.equalsIgnoreCase(uri, redirectLocation)) {
-//            return true;
-//        }
-//        HttpSession session = request.getSession();
-//        Object object = session.getAttribute("admin");
-//        if (object == null) {
-//            response.sendRedirect(redirectLocation);
-//        }
+		String uri = request.getRequestURI();
+		String redirectLocation = request.getContextPath()+"/admin/login";
+        if (StringUtils.equalsIgnoreCase(uri, redirectLocation)) {
+            return true;
+        }
+        HttpSession session = request.getSession();
+        Object object = session.getAttribute("admin");
+        if (object == null) {
+            response.sendRedirect(redirectLocation);
+        }
         return true;
     }
 
